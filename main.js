3cbce2da1686985ed0f18c3b47cf5543cd75f881
lubigsis <lucre.baluc@gmail.com>
2023-03-01 01:30:49 -0300

    Agregué función mover hacia la izquierda las imágenes

--- main.js
+++ main.js
@@ -14,17 +14,49 @@ slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function moverDerecha(){
     let sliderSectionFirst = document.querySelectorAll('.slider-seccion')[0];//obtengo el primero
+
     slider.style.marginleft = '-200%'; //ver CSS 
     slider.style.transition = 'all 0.5s';
 
-    setTimeout(function(){
-        slider.style.transition = 'none';
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginleft = '-100%';
+    setTimeout(function(){  /*Esto es pq no puedo hacer los estilos de arriba a la misma vez*/
+        slider.style.transition = 'none'; /*se la saco la transición pq este movimiento, de pasar de ser la primera img a la última, es interno. El user no ve este funcionamiento interno*/
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);/*posición donde coloco internamente la img uno (antes q termine el slider)*/
+        slider.style.marginleft = '-100%'; /*c/este valor se lo regresa a la normalidad, a su verdadero tamaño*/
 
     }, 500); //el equivalente a medio minuto q' coloqué arriba.
 }
 
+
+
+
+function moverIzquierda(){
+    let sliderSection = document.querySelectorAll('.slider-seccion');/*tengo q' volver a declarar las variables pq c/vez q' se da un click, ese elemento varía*/
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+    slider.style.marginleft = '0'; 
+    slider.style.transition = 'all 0.5s';
+
+    setTimeout(function(){  
+        slider.style.transition = 'none'; 
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginleft = '-100%'; 
+
+    }, 500); 
+}
+
+
+
 btnRight.addEventListener('click', function(){
     moverDerecha();
 });
+
+
+btnLeft.addEventListener('click', function(){
+    moverIzquierda();
+});
+
+
+/*------------------------------------para hacer el slider manual--------------------------*/
+setInterval(function(){
+    moverDerecha();
+}, 5000); /*5"*/
